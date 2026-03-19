8d806c736c36cd15d25350588c725a804b231ecc
morio <moriohamada@gmail.com>
2026-03-19 11:39:43 +0000

    lazy import pyarrow for hpc compatibility

--- utils/filing.py
+++ utils/filing.py
@@ -3,11 +3,11 @@ useful functions for navigating directories, getting paths for relevant files et
 """
 import os
 import pandas as pd
-import pyarrow.parquet as pq
 
 
 def load_fr_matrix(path) -> pd.DataFrame:
     """Load large FR_matrix parquet files"""
+    import pyarrow.parquet as pq
     table = pq.read_table(str(path), thrift_string_size_limit=1000000000)
     return table.to_pandas()
 
