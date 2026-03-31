84e369a4a87175c3cf258af26b3806396cb69e6e
morio <moriohamada@gmail.com>
2026-03-31 14:55:04 +0100

    updated bash scripts for network model submissions

--- neuron_prediction/network/submit_pt2.bash
+++ neuron_prediction/network/submit_pt2.bash
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH -t 0-06:00
-#SBATCH --array=0-9999%100
+#SBATCH --array=0-5405%100
 
 module load miniconda/23.10.0
 source $(conda info --base)/etc/profile.d/conda.sh
@@ -16,5 +16,5 @@ CODE_DIR=/nfs/nhome/live/morioh/Documents/PycharmProjects/dmdm_npxAnalysis
 cd $CODE_DIR
 
 python -u -m neuron_prediction.network.run_neuron \
-    --job-index $SLURM_ARRAY_TASK_ID \
+    --job-index $((SLURM_ARRAY_TASK_ID + 10000)) \
     --job-map /ceph/mrsic_flogel/public/projects/MoHa_20260212_dmdmTemporalExpectation/data/npx/network_job_map.csv
