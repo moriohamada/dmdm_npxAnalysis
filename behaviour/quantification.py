001837d575f81852ee680ccd69e3507efaad479f
morio <moriohamada@gmail.com>
2026-05-08 10:43:47 +0100

    just for computer transfer - minor start to behavioural quantifcation fns

--- behaviour/quantification.py
+++ behaviour/quantification.py
@@ -6,12 +6,42 @@ from config import ANALYSIS_OPTIONS
 from behaviour.extraction import load_behavioural, save_behavioural
 import numpy as np
 
+CHANGE_TFS = np.log2(ANALYSIS_OPTIONS['change_tfs'])
+
+def _fit_psychometric(hit_rates, changes: list[float] = CHANGE_TFS):
+    pass
+
+def _quantify_performance_change(hit_rates, changes, change_labels):
+    """
+    singned rank/permutation test on differences in hit rates or rts; also hit:fa ratios
+    """
+    pass
+
 def quantify_psychometrics(psycho: np.ndarray,
                            chrono: np.ndarray,
                            config=ANALYSIS_OPTIONS):
     """
     shape: animals x change_TF x block (early/late) x probe (expected/unexpected timing)
+
+    Test difference between psychometric (params?) for early vs late blocks, early in trial.
     """
+    psycho_E = psycho[:, :, 0, 0]
+    psycho_L = psycho[:, :, 1, 1]
+    chrono_E = chrono[:, :, 0, 0]
+    chrono_L = chrono[:, :, 1, 1]
+
+    psychometric_params = {'early': _fit_psychometric(psycho_E),
+                           'late': _fit_psychometric(psycho_L)}
+
+    # compute delta and signf by change size (zero/small/large - anova?)
+    psychometric_stats = {'early': {},
+                          'late': {}}
+
+
+
+
+
+
 
 
 def quantify_pulse_lick_probability():
